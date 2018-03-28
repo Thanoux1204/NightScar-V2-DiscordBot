774dedfca2e6a55bf38b4292ef0d91a03e6d4a92
Thanoux1204 <34548741+Thanoux1204@users.noreply.github.com>
2018-03-28 14:43:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -113,9 +113,9 @@ bot.on('message', message => {
     }
 
     bot.on('message', message => {
-        if(message.content.startsWith(prefix +"sondages")) {
+        if(message.content.startsWith(prefix +"sondage")) {
             if(message.author.id == '304308896581812246') {
-                let args = messgae.content.split(" ").slice(1);
+                let args = message.content.split(" ").slice(1);
                 let thingToEcho = args.join(" ")
                 var embed = new Discord.RichEmbed()
                     .setDescription("Sondage")
