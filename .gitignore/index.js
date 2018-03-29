c4aa229276a5cb129f126a727a70c97c0dfc77d1
Thanoux1204 <34548741+Thanoux1204@users.noreply.github.com>
2018-03-29 19:08:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -25,15 +25,7 @@ bot.login(process.env.TOKEN);
 bot.on('message', message => {
     if (message.content === prefix + "help"){
 	  message.channel.sendMessage("/!\ MAINTENANCE EN COURS /!\");
-       // message.channel.sendMessage("📩 la page d'aide vous a été envoyé en message privé ! 👍")
-        //var embed = new Discord.RichEmbed()
-          //  .setTitle("=-=-=-=-=-: PAGE D'AIDE :-=-=-=-=-=")
-            //.setDescription("Page de la liste des commande disponible du bot")
-            //.addField("- Help", "Affiche la liste des commandes du bot disponible", true)
-            //.addField("- infobot", "Affiche les infos concernant le bot", false)
-            //.setColor("0xe78501")
-            //.setFooter("Bon jeu parmis nous ^^")
-            //message.author.sendEmbed(embed);
+     
     }
 
 
